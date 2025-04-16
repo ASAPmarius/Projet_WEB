0e5dbd705a775857c756f7ad4fb4532ecf44c797
ASAP Marius <marius.poirierpro@gmail.com>
2025-04-16 16:21:10 +0200

    fix 2

--- frontend/scripts/script_create_acount.js
+++ frontend/scripts/script_create_acount.js
@@ -1,45 +1,44 @@
 // eslint-disable-next-line no-unused-vars
 function createAccount() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirm_password').value;
-    const errorMessage = document.getElementById('error-message');
-    const profilePicture = document.querySelector('input[name="profile_picture"]:checked').value;
-  
-    if (password !== confirmPassword) {
-      errorMessage.textContent = 'Passwords do not match!';
-      errorMessage.style.display = 'block';
-      return;
-    }
-  
-    // Proceed with account creation
-    fetch('http://localhost:3000/create_account', {
-      method: 'POST',
-      mode: 'cors',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        username: username,
-        password: password,
-        profilePicture: profilePicture
-      })
-    })
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Failed to create account.');
-        }
-      })
-      // eslint-disable-next-line no-unused-vars
-      .then(data => {
-        alert('Account created successfully!');
-        globalThis.location.href = '../index.html';
-      })
-      .catch(error => {
-        console.error(error);
-      });
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('confirm_password').value;
+  const errorMessage = document.getElementById('error-message');
+  const profilePicture = document.querySelector('input[name="profile_picture"]:checked').value;
+
+  if (password !== confirmPassword) {
+    errorMessage.textContent = 'Passwords do not match!';
+    errorMessage.style.display = 'block';
+    return;
   }
-  
+
+  // Proceed with account creation
+  fetch('http://localhost:3000/create_account', {
+    method: 'POST',
+    mode: 'cors',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+      profilePicture: profilePicture
+    })
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Failed to create account.');
+      }
+    })
+    // eslint-disable-next-line no-unused-vars
+    .then(data => {
+      alert('Account created successfully!');
+      globalThis.location.href = '../index.html';
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
